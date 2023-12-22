0d604cacb6fae1ced219ea7d4e765eceb3c60207
HM <hafeez.itm@gmail.com>
2023-12-22 05:16:59 -0500

    initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "rg-000-task-github-terra-azure-jenkins"
+}
